318da3597fd9989561243d7ca7d84828b57b43ec
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-11 09:06:17 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+
+const Login = () => {
+  return (
+    <main>
+    <h2>Workasana</h2>
+    <div>
+        <h1>Log in to your account</h1>
+        <p>Please enter your details</p>
+        <div>
+        <label>Email</label>
+        <input type="text" placeholder="Enter your email"/>
+        </div>
+        <div>
+        <label>Password</label>
+        <input type="password" placeholder="Password" />
+        </div>
+    </div>
+
+    </main>
+  )
+}
+
+export default Login
