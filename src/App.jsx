50c24e13be2e5accbde3fe76605ab7cac83da80a
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-13 13:13:00 +0530

    Added Vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <Authprovider>
      {/* <Navbar/> */}
       <Routes>
-        <Route path="/" element={<Login/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
          <Route element={<ProtectedRoute />}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
       </Routes>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:2000/user/login", {
+        const response = await fetch("https://asna-backend.vercel.app/user/login", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
